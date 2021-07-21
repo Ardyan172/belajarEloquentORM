97d20e7442abaa5bfa1a9dc44d3bc128884030e7
@Ardyan172 <ardyanrhm321@gmail.com>
2021-07-21 17:04:57 +0700

    Membuat fitur menambah data ke table users

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,7 +37,38 @@ class UsersController extends Controller
      */
     public function store(Request $request)
     {
-        return "OK";
+        // buat validasi
+        $validasiData = $request->validate([
+            'name' => ['required', 'unique:users', 'min:4', 'max:20'], 
+            'email' => ['required', 'unique:users', 'max:20'],
+            'password' => ['required', 'min:7', 'max:20'],
+            // index bail berfungsi menghentikan kegagalan validasi pertama
+        ], [
+            // nama
+            'name.required' => 'Kamu belum memasukkan nama',
+            'name.unique' => 'Nama ini sudah digunakan orang lain',
+            'name.min' => 'Kamu harus memasukkan minimal 4 huruf',
+            'name.max' => 'Maksimal huruf yang bisa kamu masukkan adalah 20 huruf',
+            // email
+            'email.required' => 'Kamu belum memasukkan email',
+            'email.unique' => 'Email ini sudah digunakan orang lain',
+            'email.max' => 'Maksimal huruf yang bisa kamu masukkan adalah 20 huruf',
+            // password
+            'password.required' => 'Kamu belum memasukkan password',
+            'password.min' => 'Kamu harus memasukkan minimal 6 huruf',
+            'password.max' => 'Maksimal huruf yang bisa kamu masukkan adalah 20 huruf',
+        ]);
+
+
+        // query insert laravel untuk menyimpan data ke table users
+        $users = Users::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+        
+        // Mengarahkan dengan data sesi yang di flash
+        return redirect()->route('users.index')->with('status', 'Users Baru Berhasil Ditambah');
     }
 
     /**
--- resources/views/users/formulirCreate.blade.php
+++ resources/views/users/formulirCreate.blade.php
@@ -1,47 +1,51 @@
 @extends('layout.app')
 
-@section('title', 'Users')
+@section('title', 'Create Users')
 
-@section('judulContent', 'Halaman Users')
+@section('judulContent', 'Create Users')
 
 @section('konten')
 <div class="card card-primary">
 	<div class="card-header">
-		<h3 class="card-title">Daftar Seluruh Users Yang Sudah Bergabung</h3>
+		<h3 class="card-title">Silahkan Isi Semua Data Users Di Formulir</h3>
 	</div>
 	<!-- /.card-header -->
 	<!-- form start -->
-	<form action="{{ route('users.store') }}" method="POST">
+	<form method="POST" action="{{ route('users.store') }}">
+		@csrf
 		<div class="card-body">
 			<div class="form-group">
-				<label for="exampleInputEmail1">Email address</label>
-				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+				<label for="name">Name</label>
+				<input type="text" name="name" value="{{ old('name') }}" class="@error('name') is-invalid @enderror form-control" id="name" placeholder="Masukan Nama Kamu" autocomplete="off">
+				<!-- validasi error -->
+				@error('name')
+				<div class="alert alert-danger">{{ $message }}</div>
+				@enderror
 			</div>
+
 			<div class="form-group">
-				<label for="exampleInputPassword1">Password</label>
-				<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+				<label for="exampleInputEmail1">Email</label>
+				<input type="email" name="email" value="{{ old('email') }}" class="@error('email') is-invalid @enderror form-control" id="exampleInputEmail1" placeholder="Masukan Email Kamu" autocomplete="off">
+				<!-- validasi error -->
+				@error('email')
+				<div class="alert alert-danger">{{ $message }}</div>
+				@enderror
 			</div>
+
 			<div class="form-group">
-				<label for="exampleInputFile">File input</label>
-				<div class="input-group">
-					<div class="custom-file">
-						<input type="file" class="custom-file-input" id="exampleInputFile">
-						<label class="custom-file-label" for="exampleInputFile">Choose file</label>
-					</div>
-					<div class="input-group-append">
-						<span class="input-group-text">Upload</span>
-					</div>
-				</div>
-			</div>
-			<div class="form-check">
-				<input type="checkbox" class="form-check-input" id="exampleCheck1">
-				<label class="form-check-label" for="exampleCheck1">Check me out</label>
+				<label for="password">Password</label>
+				<input type="password" name="password" class="@error('password') is-invalid @enderror form-control" id="password" placeholder="Masukan Password Kamu" autocomplete="off">
+				<!-- validasi error -->
+				@error('password')
+				<div class="alert alert-danger">{{ $message }}</div>
+				@enderror
 			</div>
 		</div>
 		<!-- /.card-body -->
 
 		<div class="card-footer">
 			<button type="submit" class="btn btn-primary">Submit</button>
+			<a href="{{ route('users.index') }}" class="btn btn-danger">Back</a>
 		</div>
 	</form>
 </div>
